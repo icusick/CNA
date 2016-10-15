ecd011693620e37d0655ce4427ba1874cffa41b1
isabel cusick <cusick.isabel@gmail.com>
2016-10-14 21:58:25 -0500

    made view pages, updated server

--- server.rb
+++ server.rb
@@ -7,15 +7,19 @@ module App
   	erb :index
   end
 
-  get "/client_list" do
+  get "/clients" do
   	@clients = Client.all
   	erb :client_list
   end	
 
   
-
-
+  get "/clients/:id" do
+  	id = params[:id]
+  	@client = Client.find(id)
+  	erb :client_view
+  end
 
 
   end
+
 end	
